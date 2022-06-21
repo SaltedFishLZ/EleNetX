1d80c7b4d98544743f704e70762327742229d788
SaltedFishLZ <zhliang@berkeley.edu>
2022-06-21 06:41:45 -0700

    PDWorkshop91 translated to JSON; needs to fix the original parser to parse HEIGHT and WIDTH keyword in .yal files

--- mcnc.py
+++ mcnc.py
@@ -1,21 +1,131 @@
 
 import os
+import json
+import subprocess
+
 
 PD91_BASE_DIR = os.path.join("mcnc", "pd91", "bench")
-PD91_YALs = {
-    # block
+
+PD91_BENCHMARKS = {
+    # block design
     "block" : {
-        "ami33", "ami49", "apte", "hp", "xerox",
+        "ami33" : {
+            "format" : ["yal", "vpnr"],
+            "technology" : "ami"
+        },
+        "ami49" : {
+            "format" : ["yal", "vpnr"],
+            "technology" : "ami"
+        },
+        "apte" : {
+            "format" : ["yal", "vpnr"],
+            # TODO: technology
+        },
+        "hp" : {
+            "format" : ["yal", "vpnr"],
+            "technology" : "ami"
+        },
+        "xerox" : {
+            "format" : ["yal", "vpnr"],
+            "technology" : "xerox"
+        },
     },
+    
     # floorplan
     "floorplan" : {
-        "fan",
+        "fan" : {
+            "format" : ["yal", "vpnr"],
+            # self-contained technology
+        },
+        # TODO: xerox
     },
+    
     # mixed
     "mixed" : {
-        "a3", "g2", "t3",
+        "a3" : {
+            "format" : ["yal", "vpnr"],
+            # self-contained technology
+        },
+        "g2" : {
+            "format" : ["yal", "vpnr"],
+            # self-contained technology
+        },
+        "t1" : {
+            "format" : ["yal", "vpnr"],
+            # self-contained technology
+        },
     },
+
+    # std cell (benchmark only, no tech lib)
+    "stdcell" : {
+        "biomed" : {
+            "format" : ["yal", "vpnr"],
+            "technology" : "db"
+        },
+        "fract" : {
+            "format" : ["yal", "vpnr"],
+            "technology" : "db"
+        },
+        "industry1" : {
+            "format" : ["yal", "vpnr"],
+            # self-contained technology
+        },
+        "industry2" : {
+            "format" : ["yal", "vpnr"],
+            # self-contained technology
+        },
+        "industry3" : {
+            "format" : ["yal", "vpnr"],
+            # self-contained technology
+        },
+        # TODO: primary1 and primary2
+        # need fix the YAL c parser to parse HEIGHT and WIDTH
+        # "primary1" : {
+        #     "format" : ["yal", "vpnr"],
+        #     "technology" : "sclib"
+        # },
+        # "primary2" : {
+        #     "format" : ["yal", "vpnr"],
+        #     "technology" : "sclib"
+        # },
+        "struct" : {
+            "format" : ["yal", "vpnr"],
+            "technology" : "db"
+        },
+    }
 }
 
-def pd91_yal2json(base_dir, bench):
-    pass
+PD91_TECHNOLOGIES = {
+    "stdcell" : {
+        "db" : {
+            "format" : ["yal", "vpnr"],
+        },
+        "sclib" : {
+            "format" : ["yal", "vpnr"],
+        },
+        # TODO: gate array
+        # need fix the YAL c parser to parse HEIGHT and WIDTH
+    }
+}
+
+def pd91_yal2json(base_dir):
+    YAL2JSON_TRANSLATOR_PATH = os.path.join("yal", "c-parser", "yal2json")
+    # translate benchmark YAL files
+    for category in PD91_BENCHMARKS:
+        for benchmark in PD91_BENCHMARKS[category]:
+            if "yal" in PD91_BENCHMARKS[category][benchmark]["format"]:
+                print("=" * 64)
+                print("translating {} from yal to json".format(benchmark))
+                print("=" * 64)
+                src_yal_path = os.path.join(PD91_BASE_DIR, category,
+                                            benchmark + ".yal")
+                dst_json_path = os.path.join(PD91_BASE_DIR, category,
+                                             benchmark + ".json")
+                subprocess.run([
+                    YAL2JSON_TRANSLATOR_PATH,
+                    src_yal_path,
+                    dst_json_path
+                ])
+
+if __name__ == "__main__":
+    pd91_yal2json(base_dir=PD91_BASE_DIR)
