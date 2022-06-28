a1d56abe2d9cfa50216473e4c3fbc61d3e7d5c38
SaltedFishLZ <zhliang@berkeley.edu>
2022-06-28 05:30:51 -0700

    try to refactor ispd98 related codes

--- ispd98.py
+++ ispd98.py
@@ -1,10 +1,15 @@
 """Parsers of ISPD98 format (.net and .are files)
 """
 import os
-try:
-    from collections.abc import OrderedDict
-except ImportError:
-    from collections import OrderedDict
+import sys
+
+if sys.version <= "3.7":
+    try:
+        from collections.abc import OrderedDict
+    except ImportError:
+        from collections import OrderedDict
+else:
+    OrderedDict = dict
 
 import networkx as nx
 import matplotlib.pyplot as plt 
@@ -12,150 +17,160 @@ import matplotlib.pyplot as plt
 from .hypergraph import hyperedges2edges
 
 
-def parse_are_file(fpath:str) -> list:
+def parse_are(file_path:str) -> OrderedDict:
     """Parse the .are file to get the area of each module
-    :param fpath: path to the .are file
-    :return: a list of nodes (dict with attr: name -> str, size -> float)
+    :param file_path: path to the .are file
+    :return: parsed nodes
     """
-    assert isinstance(fpath, str), TypeError()
+    assert isinstance(file_path, str), TypeError()
 
-    nodes = []
-    with open(fpath) as f:
+    nodes = OrderedDict()
+    with open(file_path) as f:
         for line in f:
             args = str(line).split()
             assert len(args) == 2, "Invalid input: {}".format(line)
             
-            nodes.append({
-                "name" : args[0],
-                "area" : float(args[1])
-                })
+            name, area = args
+            area = int(area)
+
+            assert name not in nodes, "Duplicated node: {}".format(name)
+            
+            nodes[name] = {
+                "area" : area
+                # no other attribute in ISPD98
+            }
 
     return nodes
 
 
-def parse_net_file(fname:str) -> list:
-    """Parse the .net file to get a hyper-graph with connections only
+def read_net_header(file_handle) -> dict:
+    """Read the header of .net file.
+    NOTE: it will start from the current pointer in the file
+    :param file_handle: opened file handle 
+    :return: a tuple of meta-data in the header
     """
+    # header format
+    # 1) num_pins :     number of pins, = \Sigma_i  #(v) if v in e_i
+    #                   is the count of all omstamces in all nets
+    # 2) num_nets:      number of nets
+    # 3) num_instances: number of instances
+    # 4) pad_offset:    offset of the pad
 
-    def read_header(f):
-        """read the header of .net file.
-        It will start from the current pointer in the file
-        :return: a tuple of meta-data in the header
-        """
-        # ignored line
-        line = f.readline()
-        # number of pins
-        line = f.readline()
-        pins_num = int(str(line))
-        # number of nets
-        line = f.readline()
-        nets_num = int(str(line))
-        # number of modules
-        line = f.readline()
-        modules_num = int(str(line))
-        # pad offset
-        line = f.readline()
-        pad_offset = int(str(line))
-        return (pins_num, nets_num, modules_num, pad_offset)
+    # ignored line
+    line = file_handle.readline()
 
-    def read_hyperedges(f, pins_num):
-        # Note: we use an ordered dict for each hyperedge
-        # key: name of node -> str
-        # value: remaining part of each line unknown meaning -> list of str
-        hyperedges = []; hyperedge_curr = OrderedDict()
+    # number of pins
+    line = file_handle.readline()
+    num_pins = int(str(line))
 
-        # scane the input file
-        for idx in range(pins_num):
-            line = f.readline()
-            args = line.split()
+    # number of nets
+    line = file_handle.readline()
+    num_nets = int(str(line))
 
-            # each data line should be in the following formats
-            # (1) <name> s 1
-            # (2) <name> l
-            # (1) indicates a driver node in a uni-directional net
-            # (2) indicates a load node in a uni-directional net
-            assert(len(args) == 2 or len(args) == 3)
+    # number of modules
+    line = file_handle.readline()
+    num_instances = int(str(line))
 
-            # start of a new hyper-edge
-            if ("s" == args[1]):
-                # push the previous hyper-edge
-                if (len(hyperedge_curr) > 0):
-                    hyperedges.append(hyperedge_curr)
-                # clear the current hyper-edge
-                hyperedge_curr = OrderedDict()
-
-            # insert current node to the current hyperedge
-            node_name = args[0]; node_attr = args[1 : ]
-            hyperedge_curr[node_name] = node_attr
-
-        # push the last hyper-edge
-        if (len(hyperedge_curr) > 0):
-            hyperedges.append(hyperedge_curr)
-
-        return hyperedges
-
-    pins_num = 0    # number of pins, = \Sigma_i  #(v) if v in e_i 
-                    # is the count of all vertices in all hyper-edges
-    nets_num = 0    # number of nets /hyper-edges
-    modules_num = 0 # number of modules/vertices
-    pad_offset = 0  # offset of the pad
-
-    hyperedges = []
-
-    with open(fname) as f:
-        # read the header
-        (pins_num, nets_num, modules_num, pad_offset) = read_header(f)
-        
-        # now, read all edges
-        hyperedges = read_hyperedges(f, pins_num)
-        
-        # sanity check
-        if (len(hyperedges) != nets_num):
-            raise ValueError(
-                "In header, there are {} nets, "
-                "while extracted {}!".format(
-                    nets_num, len(hyperedges)
-                ))
+    # pad offset
+    line = file_handle.readline()
+    pad_offset = int(str(line))
 
     return {
-        "hyperedges" : hyperedges,
-        "header": {
-            "pins_num" : pins_num,
-            "nets_num" : nets_num,
-            "modules_num" : modules_num,
-            "pad_offset" : pad_offset
-        }
+        "num_pins"      :   num_pins,
+        "num_nets"      :   num_nets,
+        "num_instances" :   num_instances,
+        "pad_offset"    :   pad_offset,
     }
 
 
-
-def parse_ispd98_to_networkx(net_fpath:str, are_fpath:str) -> nx.Graph:
+def read_net_netlist(file_handle, num_pins) -> list:
+    """Read the nets of the .net file
+    NOTE: it will start from the current pointer in the file
+    :param file_handle: opened file handle 
+    :return: a list of net objects
     """
+    netlist = []
+
+    # Scan the input file
+    #
+    # each data line should be in the following formats
+    # (1) `<name> s 1` or (2) `<name> l`
+    # (1) indicates a driver node in a uni-directional net
+    # (2) indicates a load node in a uni-directional net
+    #
+    # NOTE: we use an OrderedDict for each net
+    # key: name of connected node (str)
+    # value: remaining part of each line unknown meaning
+    curr_net = OrderedDict()
+    for idx in range(num_pins):
+        line = file_handle.readline()
+        args = line.split()
+        assert(len(args) == 2 or len(args) == 3)
+        # start of a new net
+        if ("s" == args[1]):
+            # push the previous net (if not empty)
+            if (len(curr_net) > 0):
+                netlist.append(curr_net)
+            # reset the current net
+            curr_net = OrderedDict()
+        # insert current node to the current net
+        node = args[0]; attr = args[1 : ]
+        curr_net[node] = attr
+    # push the last nnet
+    if (len(curr_net) > 0):
+        netlist.append(curr_net)
+
+    return netlist
+
+
+def parse_net(file_path:str) -> list:
+    """Parse the .net file to get a netlist and a header
+    """
+
+    with open(file_path) as f:
+        # (1) read the header
+        header = read_net_header(f)
+        # (2) read all edges
+        netlist = read_net_netlist(f, header["num_pins"])
+        # sanity check
+        num_nets = len(netlist)
+        if (num_nets != header["num_nets"]):
+            err_msg = ("In header, there should be %d nets,"
+                       "while %d extracted."
+                       % (header["num_nets"], num_nets))
+            raise ValueError(err_msg)
+
+    return {
+        "netlist"   :   netlist,
+        "header"    :   header
+    }
+
+
+def ispd98_netlist_to_hyperedges(netlist):
+    """Convert ISPD98 netlist to hyperedges
+    TODO: finalize the format for hyperedges
+    """
+    hyperedges = [list(net.keys()) for net in netlist]
+    return hyperedges
+
+
+def ispd98_to_nx(net_path:str, are_path:str) -> nx.Graph:
+    """Parse ISPD98 file to NetworkX graph
     """
     G = nx.Graph()
 
+    parsed_are = parse_are(are_path)
+    parsed_net = parse_net(net_path)
+
     # add nodes with attributes to the graph
-    # (1) parse .are file
-    parsed_are_file = parse_are_file(are_fpath)
-    nodes = [
-        # convert each node from dict to tuple
-        (node["name"], node["area"])
-        for node in parsed_are_file
-    ]
-    # (2) unzip attributes
-    names, areas = zip(*nodes)
-    # (3) add nodes to nx.Graph
-    G.add_nodes_from(names, area=areas)
+    # currently, only area attribute is available
+    nodes = parsed_are
+    G.add_nodes_from(nodes.items())
 
     # add edges with attributes to the graph
-    # (1) parse .net file
-    parsed_net_file = parse_net_file(net_fpath)
-    hyperedges = parsed_net_file["hyperedges"]
-    # (2) extract pure hyperedges with unit weight
-    hyperedges = [list(hyperedge.keys()) for hyperedge in hyperedges]
-    # (3) convert hyperedges to normal edges
+    netlist = parsed_net["netlist"]
+    hyperedges = ispd98_netlist_to_hyperedges(netlist)
     edges = hyperedges2edges(hyperedges)
-    # (4) add edges to nx.Graph
     for edge in edges:
         G.add_edge(edge["src"], edge["dst"], weight=edge["weight"])
 
@@ -170,20 +185,20 @@ if __name__ == "__main__":
     circuit_dir = "ispd98"
     circuit_name = "sample"
 
-    are_fpath = os.path.join(script_dir_path, circuit_dir,
-                             circuit_name + ".are")
-    net_fpath = os.path.join(script_dir_path, circuit_dir,
-                             circuit_name + ".net")
+    are_path = os.path.join(script_dir_path, circuit_dir,
+                            circuit_name + ".are")
+    net_path = os.path.join(script_dir_path, circuit_dir,
+                            circuit_name + ".net")
 
-    parsed_are = parse_are_file(are_fpath)
+    parsed_are = parse_are(are_path)
     print(parsed_are)
 
-    parsed_net = parse_net_file(net_fpath)
+    parsed_net = parse_net(net_path)
     print(parsed_net)
 
     import matplotlib.pyplot as plt
 
-    G = parse_ispd98_to_networkx(net_fpath, are_fpath)
+    G = ispd98_to_nx(net_path, are_path)
     print(G.nodes())
     print(G.edges())
 
